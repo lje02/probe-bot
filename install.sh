89fafa0ff6c8bacb42d7ec58602ca731c232b911
lje02 <154250786+lje02@users.noreply.github.com>
2026-05-04 10:15:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,9 @@
 # sing-box 一键脚本 (快捷方式: ssb)
 # ==========================================
 
-RED='\033[1;31m'    
-GREEN='\033[1;32m'  
-YELLOW='\033[1;33m' 
-BLUE='\033[1;34m'   
-PURPLE='\033[1;35m' 
-CYAN='\033[1;36m'   
-WHITE='\033[1;37m'  
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
 PLAIN='\033[0m'     
 
 CONFIG_FILE="/etc/sing-box/config.json"
